743b67e127a8422b48abf431e91a6eb8c1518a93
Isaac Avila <isaavila@csumb.edu>
2018-09-26 06:56:28 +0000

    add hw2

--- homework/hw2/inc/battle.php
+++ homework/hw2/inc/battle.php
@@ -10,7 +10,7 @@ function displayTypes($randomTypes, $pos){
     $typevalue = $p_types[$randomTypes];
     
     echo "<figure id = 'types'>";
-    echo "<img id='player$pos' src =\"img/$typevalue.png\" alt='$typevalue' title='". ucfirst($typevalue) ."' width='15%' >";
+    echo "<img id='player$pos' src =\"img/$typevalue.png\" alt='$typevalue' title='". ucfirst($typevalue) ."' width='30%' >";
     echo "</figure>";
 } //displayTypes()
 
@@ -18,28 +18,41 @@ function displayWinner($randomType1, $randomType2){
     global $effect;
     global $p_types;
     $attack = $effect[0];
-    $basetype1 = $p_types[0];
-    $basetype2 = $p_types[0];
+    $basetype1 = $p_types[$randomType1];
+    $basetype2 = $p_types[$randomType2];
     
     echo "<div id='output'>";
-    if($randomType1 == $randomType2){
-        switch($randomType1){
+    if($basetype1 == "fire"){
+        switch($randomType2){
             case 0: $attack = $effect[0];
-                    $basetype1 = $p_types[$randomType1];
-                    $basetype2 = $p_types[$randomType2];
-                    echo "<h3>$basetype1 types are $attack against  $basetype2 types.</h3>";
+                    echo "<h3>$basetype1 type moves are $attack against $basetype2 types.</h3>";
+                    break;
+            case 1: $attack = $effect[0];
+                    echo "<h3>$basetype1 type moves are $attack against $basetype2 types.</h3>";
+                    break;
+                    
+        }
+    } else if($basetype1 == "water"){
+        switch($randomType2){
+            case 0: $attack = $effect[2];
+                    echo "<h3>$basetype1 type moves are $attack against $basetype2 types.</h3>";
+                    break;
+            case 1: $attack = $effect[0];
+                    echo "<h3>$basetype1 type moves are $attack against $basetype2 types.</h3>";
                     break;
         }
     }
+    
     echo "</div>";
 }
 
 function play() {
     
     for($i=1; $i<3; $i++){
-        ${"randomType" . $i } = rand(0,0);
+        ${"randomType" . $i } = rand(0,1);
         displayTypes(${"randomType" . $i}, $i );
     }
+    
     displayWinner($randomType1, $randomType2);
 }
 
